35bbe7cfd07162265c08654eddc47c11b66f91b1
Kual Louis <65467434+Louis12345642@users.noreply.github.com>
2023-09-21 18:13:16 +0300

    fix the error

--- _printf.c
+++ _printf.c
@@ -9,7 +9,7 @@ void print_buffer(char buffer[], int *buff_ind);
  */
 int _printf(const char *format, ...)
 {
-	int a, p = 0, printed_chars = 0;
+	int a, p = 0, p_chars = 0;
 	int flags, width, precision, size, buff_ind = 0;
 	va_list list;
 	char buffer[BUFF_SIZE];
@@ -27,7 +27,7 @@ int _printf(const char *format, ...)
 			if (buff_ind == BUFF_SIZE)
 				print_buffer(buffer, &buff_ind);
 			/* write(1, &format[a], 1);*/
-			printed_chars++;
+			p_chars++;
 		}
 		else
 		{
@@ -41,7 +41,7 @@ int _printf(const char *format, ...)
 				flags, width, precision, size);
 			if (p == -1)
 				return (-1);
-			printed_chars += p;
+			p_chars += p;
 		}
 	}
 
@@ -49,7 +49,7 @@ int _printf(const char *format, ...)
 
 	va_end(list);
 
-	return (printed_chars);
+	return (p_chars);
 }
 
 /**
