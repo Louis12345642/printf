ed7b0041da4afc60e3a2f735f7626432b5e75953
Mubarak kual louis <mubaraklouis@gmail.com>
2023-09-22 22:38:19 +0300

    finish width

--- get_size.c
+++ get_size.c
@@ -10,18 +10,18 @@
  */
 int get_size(const char *format, int *i)
 {
-	int curr_i = *i + 1;
+	int c = *i + 1;
 	int size = 0;
 
-	if (format[curr_i] == 'l')
+	if (format[c] == 'l')
 		size = S_LONG;
-	else if (format[curr_i] == 'h')
+	else if (format[c] == 'h')
 		size = S_SHORT;
 
 	if (size == 0)
-		*i = curr_i - 1;
+		*i = c - 1;
 	else
-		*i = curr_i;
+		*i = c;
 
 	return (size);
 }
--- get_width.c
+++ get_width.c
@@ -1,28 +1,28 @@
 #include "main.h"
 
 /**
- * get_width - Calculates the width for printing
- * @format: Formatted string in which to print the arguments.
- * @i: List of arguments to be printed.
- * @list: list of arguments.
+ * get_width - finds the width
+ * @format: formating.
+ * @i: parametre.
+ * @list: parameters list.
  *
  * Return: width.
  */
 int get_width(const char *format, int *i, va_list list)
 {
-	int curr_i;
+	int c;
 	int width = 0;
 
-	for (curr_i = *i + 1; format[curr_i] != '\0'; curr_i++)
+	for (c = *i + 1; format[c] != '\0'; c++)
 	{
-		if (is_digit(format[curr_i]))
+		if (is_digit(format[c]))
 		{
 			width *= 10;
-			width += format[curr_i] - '0';
+			width += format[c] - '0';
 		}
-		else if (format[curr_i] == '*')
+		else if (format[c] == '*')
 		{
-			curr_i++;
+			c++;
 			width = va_arg(list, int);
 			break;
 		}
@@ -30,7 +30,7 @@ int get_width(const char *format, int *i, va_list list)
 			break;
 	}
 
-	*i = curr_i - 1;
+	*i = c - 1;
 
 	return (width);
 }
