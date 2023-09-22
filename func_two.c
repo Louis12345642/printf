8bc42c0447158082320bba3bde43dd891376a477
Mubarak kual louis <mubaraklouis@gmail.com>
2023-09-22 21:46:31 +0300

    finsh the pointer funtion

--- func_two.c
+++ func_two.c
@@ -14,42 +14,42 @@
 int print_pointer(va_list types, char buffer[],
 	int flags, int width, int precision, int size)
 {
-	char extra_c = 0, padd = ' ';
-	int ind = BUFF_SIZE - 2, length = 2, padd_start = 1; /* length=2, for '0x' */
-	unsigned long num_addrs;
-	char map_to[] = "0123456789abcdef";
-	void *addrs = va_arg(types, void *);
+	char add_c = 0, p = ' ';
+	int i = BUFF_SIZE - 2, len = 2, start = 1; /* length=2, for '0x' */
+	unsigned long n_adress;
+	char map[] = "0123456789abcdef";
+	void *address = va_arg(types, void *);
 
 	UNUSED(width);
 	UNUSED(size);
 
-	if (addrs == NULL)
+	if (address == NULL)
 		return (write(1, "(nil)", 5));
 
 	buffer[BUFF_SIZE - 1] = '\0';
 	UNUSED(precision);
 
-	num_addrs = (unsigned long)addrs;
+	n_adress = (unsigned long)address;
 
-	while (num_addrs > 0)
+	while (n_adress > 0)
 	{
-		buffer[ind--] = map_to[num_addrs % 16];
-		num_addrs /= 16;
-		length++;
+		buffer[i--] = map[n_adress % 16];
+		n_adress /= 16;
+		len++;
 	}
 
 	if ((flags & F_ZERO) && !(flags & F_MINUS))
-		padd = '0';
+		p = '0';
 	if (flags & F_PLUS)
-		extra_c = '+', length++;
+		add_c = '+', len++;
 	else if (flags & F_SPACE)
-		extra_c = ' ', length++;
+		add_c = ' ', len++;
 
-	ind++;
+	i++;
 
 	/*return (write(1, &buffer[i], BUFF_SIZE - i - 1));*/
-	return (write_pointer(buffer, ind, length,
-		width, flags, padd, extra_c, padd_start));
+	return (write_pointer(buffer, i, len,
+		width, flags, p, add_c, start));
 }
 
 /*******************print non printable**************/
