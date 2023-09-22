6f1458c433a8e3663f1519fe013eb48bc193dab4
Mubarak kual louis <mubaraklouis@gmail.com>
2023-09-22 22:35:03 +0300

    finish the size

--- get_size.c
+++ get_size.c
@@ -2,11 +2,11 @@
 #include "main.h"
 
 /**
- * get_size - Calculates the size to cast the argument
- * @format: Formatted string in which to print the arguments
- * @i: List of arguments to be printed.
+ * get_size - finds the size
+ * @format: use for string formating
+ * @i: show list of parametres.
  *
- * Return: Precision.
+ * Return: precision.
  */
 int get_size(const char *format, int *i)
 {
